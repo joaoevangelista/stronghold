9a4368469d99d057901a5d5b8e115e5e9e3ef2a2
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-06-29 19:00:05 -0300

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629183309) do
+ActiveRecord::Schema.define(version: 20160629193622) do
+
+  create_table "activities", force: :cascade do |t|
+    t.integer  "trackable_id"
+    t.string   "trackable_type"
+    t.integer  "owner_id"
+    t.string   "owner_type"
+    t.string   "key"
+    t.text     "parameters"
+    t.integer  "recipient_id"
+    t.string   "recipient_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type"
+  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type"
+  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type"
+
+  create_table "announcements", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.integer  "user_id"
+    t.boolean  "notify"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "announcements", ["user_id"], name: "index_announcements_on_user_id"
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -33,16 +61,17 @@ ActiveRecord::Schema.define(version: 20160629183309) do
 
   create_table "issues", force: :cascade do |t|
     t.string   "title"
-    t.string   "content",     default: ""
-    t.boolean  "is_resolved", default: false, null: false
+    t.string   "description"
+    t.boolean  "is_resolved"
     t.integer  "user_id"
     t.date     "due_date"
     t.integer  "assignee_id"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.integer  "issue_type_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
-  add_index "issues", ["assignee_id"], name: "issues_assigned_to"
+  add_index "issues", ["issue_type_id"], name: "index_issues_on_issue_type_id"
   add_index "issues", ["user_id"], name: "index_issues_on_user_id"
 
   create_table "users", force: :cascade do |t|
