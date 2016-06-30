0a73ae31071c04dd3c44e7a04a178bbce4269331
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-06-29 22:38:42 -0300

    Fix edit form and root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
 
-  get '/' => 'home#index', as: :root_url
+  get '/' => 'home#index', as: :root_path
 
   get 'activities' => 'activity#index', as: :activities_path
 
