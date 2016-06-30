a1045c187b02046f1fab77bbb66de43eafaf2004
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-06-30 13:44:32 -0300

    Add login user at ann. req. spec

--- spec/requests/announcements_spec.rb
+++ spec/requests/announcements_spec.rb
@@ -2,6 +2,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Announcements', type: :request do
+  login_user
   describe 'GET /announcements' do
     it 'works! (now write some real specs)' do
       get announcements_path
