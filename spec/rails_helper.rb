cfd55e61d8896ce353ca713fa4b9286207c97d77
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-06-30 14:32:52 -0300

    Fix controller macros appliable types

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,8 +35,7 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, type: :controller
   config.include Devise::TestHelpers, type: :view
-  config.extend ControllerMacros, type: :controller
-  config.extend ControllerMacros, type: :view
+  config.extend ControllerMacros
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
