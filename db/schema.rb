49082126c9d9cc56857cb398ae31fc6469a453de
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-06-29 22:34:31 -0300

    Add sign up sign in and edit registration layouts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629193622) do
+ActiveRecord::Schema.define(version: 20160630012726) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20160629193622) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
