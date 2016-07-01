e3f3e1c81169caf7d3abca5ffb46e4d2fb5efdad
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-01 17:40:04 -0300

    Add tracked owner custom value to be current_user

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 class Announcement < ActiveRecord::Base
   include PublicActivity::Model
-  tracked
-
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
+  
   validates :user, :title, :description, presence: true
 
   belongs_to :user
--- app/models/issue_type.rb
+++ app/models/issue_type.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 class IssueType < ActiveRecord::Base
   include PublicActivity::Model
-  tracked
-
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
+  
   validates :name, presence: true
   validates :color, color: true
 
