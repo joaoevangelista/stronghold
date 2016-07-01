fc7958acd3a63318ff3085394a4c297c0e8048f1
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-06-30 22:07:54 -0300

    Add show page to events

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
     hash = Digest::MD5::hexdigest email
     "http://gravatar.com/avatar/#{hash}.png?s=#{size}"
   end
+
+  def date_time(str)
+    return '' if not str
+    str.strftime('%b %d, %Y - %H:%M')
+  end
+
+  def date(str)
+    return '' if not str
+    str.strftime('%b %d, %Y')
+  end
 end
