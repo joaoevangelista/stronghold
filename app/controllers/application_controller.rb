a8bfa84be8363093089d661c95f8a8d31dad748e
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-06 20:25:02 -0300

    Fix pundit typo and add datetime class for mobiles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
   def user_not_authorized(exception)
     policy_name = exception.policy.class.to_s.underscore
-    flash[:error_notice] = t("#{policy_name}.#{exception.query}", scope: 'pudint',
+    flash[:error_notice] = t("#{policy_name}.#{exception.query}", scope: 'pundit',
                                                                   default: :default)
     redirect_to(request.referrer || root_path)
   end
