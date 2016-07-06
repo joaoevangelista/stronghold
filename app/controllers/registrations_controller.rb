d59ddc45d9ffab64ab0713ccb9c0eca51faa3de1
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-06 17:12:33 -0300

    Override devise registration methods to add authorize

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,27 @@
 # frozen_string_literal: true
 # Override of Devise controller to customize the permited fields
 class RegistrationsController < Devise::RegistrationsController
+
+  # Override devise create method and add code before it
+  def create
+    build_resource(sign_up_params)
+    authorize resource, :create?
+    super
+  end
+
+  def new
+    build_resource({})
+    authorize resource, :new?
+    super
+  end
+
   def sign_up_params
     params.require(:user).permit(:email, :name, :password,
-                                 :password_confirmation, :current_password)
+    :password_confirmation, :current_password)
   end
 
   def account_update_params
     params.require(:user).permit(:email, :name, :password,
-                                 :password_confirmation, :current_password)
+    :password_confirmation, :current_password)
   end
 end
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
 # Policies for user
 class UserPolicy < ApplicationPolicy
 
+  # Only managers has the hability to create users
+  def create?
+    @user.has_role? :manager
+  end
 end
