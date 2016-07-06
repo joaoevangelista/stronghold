a044af46bf0790fef61bcacf0e9a0815afa8321c
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-06 13:31:57 -0300

    Fix :optional key issue

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,8 +4,8 @@ class Role < ActiveRecord::Base
   has_and_belongs_to_many :users, join_table: :users_roles
 
   belongs_to :resource,
-             polymorphic: true,
-             optional: true
+             polymorphic: true
+            # optional: true Only Rails 5 supports
 
   validates :resource_type,
             inclusion: { in: Rolify.resource_types },
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!({name: 'Adminstration', email: 'admin@stronghold.com', password: '123456' })
+user = User.create!({name: 'Adminstration', email: 'admin@stronghold.com', password: '123456' })
+user.add_role :manager
