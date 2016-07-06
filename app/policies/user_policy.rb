b01aeeae6b13c3dc334d24388ecdb2f5dbd41471
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-06 19:39:32 -0300

    Add role to sign up form

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 # Policies for user
 class UserPolicy < ApplicationPolicy
-
   # Only managers has the hability to create users
   def create?
     @user.has_role? :manager
