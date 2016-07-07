1658462aae4ea68df8501314fbf21f02d74c3eae
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-07 15:10:43 -0300

    Add vote model

---
+++ app/models/vote.rb
@@ -0,0 +1,26 @@
+# A vote of an user on an issue
+class Vote < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :issue
+
+  # Add a vote to the specific issue on behalf of user
+  def self.upvote(user, issue)
+    vote = Vote.find_by user: user, issue: issue
+    unless vote
+     new_vote = Vote.new{user: user, issue: issue}
+     new_vote.save
+   else
+     false
+    end
+  end
+
+  # Remove the vote of an user in at specific issue
+  def self.unvote(user, issue)
+    vote = Vote.find_by user: user, issue: issue
+    if vote
+      return vote.destroy
+    else
+      false
+    end
+  end
+end
