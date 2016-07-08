8e5f0de28abc1989e091dd7d2501e73f62b05653
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-08 01:54:21 -0300

    Fix rails helper includes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,13 +16,20 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include FactoryGirl::Syntax::Methods
-  config.include Devise::TestHelpers, type: :controller
-  config.include Devise::TestHelpers, type: :view
-  config.include Devise::TestHelpers, type: :request
   config.include PolicyMacros, type: :view
-  config.extend ControllerMacros, type: :controller
-  config.extend ControllerMacros, type: :view
-  config.extend ControllerMacros, type: :request
+  [:controller, :view, :request].each do |type|
+   config.include ::Rails::Controller::Testing::TestProcess, :type => type
+   config.include ::Rails::Controller::Testing::TemplateAssertions, :type => type
+   config.include ::Rails::Controller::Testing::Integration, :type => type
+   config.include Devise::Test::ControllerHelpers, :type => type
+   config.include Devise::Test::ControllerHelpers, :type => type
+   config.include Devise::Test::ControllerHelpers, :type => type
+   config.extend ControllerMacros, :type => type
+   config.extend ControllerMacros, :type => type
+   config.extend ControllerMacros, :type => type
+ end
+
+
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
