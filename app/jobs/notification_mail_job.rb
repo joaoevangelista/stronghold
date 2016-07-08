c12273f64f4af987328a720c9b757c61b5b354d5
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-08 00:59:06 -0300

    Inherit jobs from ApplicationJob

--- app/jobs/notification_mail_job.rb
+++ app/jobs/notification_mail_job.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 # Job adapter to send email rightaway on background
-class NotificationMailJob < ActiveJob::Base
+class NotificationMailJob < ApplicationJob
   queue_as :default
 
   def perform(announcement)
