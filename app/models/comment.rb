fa80f9e1fd99a41e8fe0fecdb572204275312771
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-09 00:46:08 -0300

    Add public activity support for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 class Comment < ApplicationRecord
+  include PublicActivity::Model
+  tracked owner: proc { |controller, _model| controller.current_user }
+  
   validates :content, presence: true
 
   belongs_to :user
