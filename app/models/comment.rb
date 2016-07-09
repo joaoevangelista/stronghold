b213b2f5526fda65464ec218e729abbad31b2c39
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-09 00:50:17 -0300

    Polish

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@
 class Comment < ApplicationRecord
   include PublicActivity::Model
   tracked owner: proc { |controller, _model| controller.current_user }
-  
+
   validates :content, presence: true
 
   belongs_to :user
