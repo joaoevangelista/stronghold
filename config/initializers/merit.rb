a017e254cbb0757fe62a06e5d2634f69064d08c8
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-11 15:57:10 -0300

    Add view and translations to badges view

--- config/initializers/merit.rb
+++ config/initializers/merit.rb
@@ -20,22 +20,22 @@ Merit.setup do |config|
 end
 
 
-observer = {id: 1, name: 'observer', description: 'Reported an issue'}
-louder = {id: 2, name: 'louder', description: 'Made an announcement'}
-first_class = {id: 3, name: 'first_class', description: 'Read 10 announcements'}
-opinator = {id: 4, name: 'opinator', description: 'Commented on five different issues'}
-any_voice_matters = {id: 5, name: 'any_voice_matters', description: 'Commented on an issue'}
+observer = {id: 1, name: 'observer', description: 'Reported an issue', level: 'bronze'}
+louder = {id: 2, name: 'louder', description: 'Made an announcement', level: 'bronze'}
+first_class = {id: 3, name: 'first_class', description: 'Read 10 announcements', level: 'silver'}
+opinator = {id: 4, name: 'opinator', description: 'Commented on five different issues', level: 'silver'}
+any_voice_matters = {id: 5, name: 'any_voice_matters', description: 'Commented on an issue', level: 'bronze'}
 taking_responsibility = {id: 7, name: 'taking_responsibility',
-  description: 'Be assigned ten times on issues'
+  description: 'Be assigned ten times on issues', level: 'silver'
 }
-# ticking = {id: 8, name: 'ticking', description: 'Set due date on fifteen issues'}
-watchful = {id: 9, name: 'watchful', description: 'Report tweenty issues'}
-say_yes = {id: 10, name: 'say_yes', description: 'Voted on ten issues'}
+# ticking = {id: 8, name: 'ticking', description: 'Set due date on fifteen issues', level:}
+watchful = {id: 9, name: 'watchful', description: 'Report tweenty issues', level: 'gold'}
+say_yes = {id: 10, name: 'say_yes', description: 'Voted on ten issues', level: 'silver'}
 #you_say_it_you_get_it = {id: 11, name: 'you_say_it_you_get_it',
-#  description: 'Close tweenty issues'
+#  description: 'Close tweenty issues, level:'
 #}
-changes = {id: 12, name: 'changes', description: 'Read tweenty five announcements'}
-#polishing = {id: 13, name: 'polishing', description: 'Edited tweenty five different issues'}
+changes = {id: 12, name: 'changes', description: 'Read tweenty five announcements', level: 'gold'}
+#polishing = {id: 13, name: 'polishing', description: 'Edited tweenty five different issues', level:}
 
 [observer, louder, first_class, opinator,
   any_voice_matters, taking_responsibility,
