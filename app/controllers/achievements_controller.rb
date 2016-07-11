ce97eb7e3e7ae37e2f7c88a76fb0f51724241373
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-07-11 15:38:47 -0300

    Add list all badges view

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,7 +1,13 @@
-class AchievementsController < ApplicationController
+class AchievementsController < AuthenticatedController
+
+  # GET /achievements
+
   def index
+    @achievements = current_user.badges
   end
 
+  # GET /achievements/1
   def show
+    @users = Merit::Badge.find(params[:id]).users
   end
 end
